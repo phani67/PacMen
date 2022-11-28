d5fa4f5aded4bcebec02e8261645d262cf2359d5
phani67 <80427190+phani67@users.noreply.github.com>
2022-11-28 15:35:38 -0600

    Add files via upload

--- pacmen.js
+++ pacmen.js
@@ -3,8 +3,9 @@ const pacArray = [
   ['./images/PacMan1.png', './images/PacMan2.png'],
   ['./images/PacMan3.png', './images/PacMan4.png'],
 ];
-var direction = 0;
+
 const pacMen = [];
+
 
 function setToRandom(scale) {
   return {
@@ -15,13 +16,15 @@ function setToRandom(scale) {
 // Factory to make a PacMan
 function makePac() {
   // returns an object with values scaled {x: 33, y: 21}
-  let velocity = setToRandom(10);
-  let position = setToRandom(200);
+  let velocity = setToRandom(100);
+  let position = setToRandom(300);
   // Add image to div id = game
   let game = document.getElementById('game');
   let newimg = document.createElement('img');
+  let direction = new Boolean(false);
+  let focus = 0;
   newimg.style.position = 'absolute';
-  newimg.src = './images/PacMan1.png';
+  newimg.src = pacArray[Number(direction)][focus];
   newimg.width = 100;
   newimg.style.left = position.x;
   newimg.style.top = position.y;
@@ -31,6 +34,8 @@ function makePac() {
     position,
     velocity,
     newimg,
+    direction,
+    focus
   };
 }
 
@@ -38,21 +43,31 @@ function update() {
   //loop over pacmen array and move each one and move image in DOM
   pacMen.forEach((item) => {
     checkCollisions(item);
+    item.focus = (item.focus + 1) % 2;
+    item.focus = item.focus;
+    item.direction = item.direction;
+    //item.newimg.focus = (item.newimg.focus + 1) % 2;
+    item.newimg.src = pacArray[Number(item.direction)][item.focus];
+    //item.newimg.src = pacArray[Number(item.newimg.direction)][item.newimg.focus];
     item.position.x += item.velocity.x;
     item.position.y += item.velocity.y;
 
     item.newimg.style.left = item.position.x;
     item.newimg.style.top = item.position.y;
   });
-  setTimeout(update, 20);
+  setTimeout(update, 200);
 }
 
 function checkCollisions(item) {
   if (
     item.position.x + item.velocity.x + item.newimg.width > window.innerWidth ||
     item.position.x + item.velocity.x < 0
-  )
+  ){
     item.velocity.x = -item.velocity.x;
+    if(item.velocity.x <0) item.direction = 1
+    else item.direction = 0;
+    item.direction = item.direction;
+  }
   if (
     item.position.y + item.velocity.y + item.newimg.height > window.innerHeight ||
     item.position.y + item.velocity.y < 0
